11ce8eaf47bd18448f8166ca04a250cf236257a1
Jordan Scales <scalesjordan@gmail.com>
2013-01-06 20:42:53 -0500

    to unclutter global namespace, added functionality for an  hash instead of using data attributes

--- dynamo.js
+++ dynamo.js
@@ -1,28 +1,28 @@
 (function($) {
 
-    $.fn.dynamo = function() {
+    $.fn.dynamo = function(options) {
 
         return this.each(function(i, v) {
-            v = $(v);
+            var v = $(v);
             // we mark launched dynamos as "intialized" then check so we don't initialize them twice
             if (v.data('initialized') == 'true')
                 return;
             
-            delay = parseInt(v.data('delay')) || 3000;
-            speed = parseInt(v.data('speed')) || 350;
-            pause = v.data('pause') || false;
-            lines = v.data('lines').split(v.data('delimiter') || ',');
+            var delay = options.delay || parseInt(v.data('delay')) || 3000;
+            var speed = options.speed || parseInt(v.data('speed')) || 350;
+            var pause = options.pause || v.data('pause') || false;
+            var lines = options.lines || v.data('lines').split(v.data('delimiter') || ',');
 
             // wrap the original contents in a span
             v.html($('<span></span>').text(v.text())).data('initialized', 'true');
 
             // grab the width of the span
-            max = v.find('span:eq(0)').width();
+            var max = v.find('span:eq(0)').width();
 
             // for each item in data-lines, create a span with item as its content
             // compare the width of this span with the max
             for (k in lines) {
-                span = $('<span></span>').text(lines[k]);
+                var span = $('<span></span>').text(lines[k]);
                 v.append(span);
                 max = Math.max(max, span.width());
             }
@@ -36,7 +36,7 @@
             });
 
             // set the height of the dynamo container
-            height = v.find('>:first-child').height();
+            var height = v.find('>:first-child').height();
 
             // style
             v.width(max)
@@ -54,8 +54,8 @@
                 v.css('text-align', 'center');
 
             // now, animate it
-            transition = function() {
-                v.dynamo_trigger();
+            var transition = function() {
+                v.dynamo_trigger({ speed: speed });
             };
 
             if (!pause) {
@@ -64,16 +64,15 @@
         });
     };
     
-    $.fn.dynamo_trigger = function() {
+    $.fn.dynamo_trigger = function(options) {
         return this.each(function(i, v) {
-            speed = parseInt($(v).data('speed')) || 350;
-            $(v).find('div:first').slideUp(speed, function() { 
+            $(v).find('div:first').slideUp(options.speed, function() { 
                 $(v).append($(this).show());
             });
         });
     };
 
     // automatically initiate cycles on elements of class 'dynamo'
-    $('.dynamo').dynamo();
+    $('.dynamo').dynamo({});
 
 })(jQuery);
