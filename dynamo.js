7a317f4f398545705305a61d8227d8cc8ba78589
Jordan Scales <scalesjordan@gmail.com>
2013-05-04 16:34:30 -0400

    better convention for variable names

--- dynamo.js
+++ dynamo.js
@@ -5,67 +5,69 @@
         return this.each(function(i, v) {
             options = options || {};
 
-            var v = $(v);
+            // select the element v
+            var v$ = $(v);
+
             // we mark launched dynamos as "intialized" then check so we don't initialize them twice
-            if (v.data('initialized') == 'true')
+            if (v$.data('initialized') == 'true')
                 return;
             
-            var delay = options.delay || parseInt(v.data('delay')) || 3000;
-            var speed = options.speed || parseInt(v.data('speed')) || 350;
-            var pause = options.pause || v.data('pause') || false;
-            var lines = options.lines || v.data('lines').split(v.data('delimiter') || ',');
-            var callback = options.callback || v.data('callback') || function() {};
+            var delay = options.delay || parseInt(v$.data('delay')) || 3000;
+            var speed = options.speed || parseInt(v$.data('speed')) || 350;
+            var pause = options.pause || v$.data('pause') || false;
+            var lines = options.lines || v$.data('lines').split(v$.data('delimiter') || ',');
+            var callback = options.callback || v$.data('callback') || function() {};
 
             // wrap the original contents in a span
-            v.html($('<span></span>').text(v.text())).data('initialized', 'true');
+            v$.html($('<span></span>').text(v$.text())).data('initialized', 'true');
 
             // grab the width of the span
-            var max = v.find('span:eq(0)').width();
+            var max = v$.find('span:eq(0)').width();
 
             // for each item in data-lines, create a span with item as its content
             // compare the width of this span with the max
             for (k in lines) {
                 var span = $('<span></span>').text(lines[k]);
 
-                v.append(span);
+                v$.append(span);
                 max = Math.max(max, span.width());
             }
 
-            // replace all the spans with inline-div's
-            v.find('span').each(function(i, ele) {
-                s = $(ele).remove();
-                d = $('<div></div>').text(s.text());
+            // replace all the spans with inline-div$'s
+            v$.find('span').each(function(i, ele) {
+                var old$ = $(ele).remove();
+                var div$ = $('<div></div>').text(old$.text());
 
                 if (!i) {
                     // our last element gets tagged
-                    d.data('trigger', 'true');
+                    div$.data('trigger', 'true');
                 }
 
-                d.width(max);
-                v.append(d);
+                div$.width(max);
+                v$.append(div$);
             });
 
             // set the height of the dynamo container
-            var height = v.find('>:first-child').height();
+            var height = v$.find('>:first-child').height();
 
             // style
-            v.width(max)
-             .height(height)
-             .css({
-                 'display' : 'inline-block',
-                 'position' : 'relative',
-                 'overflow' : 'hidden',
-                 'vertical-align' : 'bottom',
-                 'text-align' : 'left'
-             });
+            v$.width(max)
+              .height(height)
+              .css({
+                  'display' : 'inline-block',
+                  'position' : 'relative',
+                  'overflow' : 'hidden',
+                  'vertical-align' : 'bottom',
+                  'text-align' : 'left'
+              });
 
             // manually center it if we need to
-            if (v.data('center'))
-                v.css('text-align', 'center');
+            if (v$.data('center'))
+                v$.css('text-align', 'center');
 
             // now, animate it
             var transition = function() {
-                v.dynamo_trigger({ speed: speed, callback: callback });
+                v$.dynamo_trigger({ speed: speed, callback: callback });
             };
 
             if (!pause) {
@@ -79,14 +81,15 @@
         return this.each(function(i, v) {
             options = options || {}
 
-            var speed = options.speed || $(v).data('speed') || 350;
-            var callback = options.callback || $(v).data('callback') || function() {};
-            $(v).find('div:first').slideUp(speed, function() {
-                $(v).append($(this).show());
+            var v$ = $(v);
+            var speed = options.speed || v$.data('speed') || 350;
+            var callback = options.callback || v$.data('callback') || function() {};
+
+            v$.find('div:first').slideUp(speed, function() {
+                v$.append($(this).show());
 
                 // check if the first item has made its way to the top again
-                // console.log($(v).find('div:first').data('trigger'));
-                if ($(v).find('div:first').data('trigger') == 'true')
+                if (v$.find('div:first').data('trigger') == 'true')
                     eval(callback).call();
             });
         });
